12bfbc773e509b5118df106e8bc37ab759230912
JJH <jjh_8416@naver.com>
2025-07-01 18:04:53 +0900

    commit_2

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/react_1" element={<Home />} />
         <Route path="/movie/:id" element={<Detail />} />        
       </Routes>
     </Router>  
